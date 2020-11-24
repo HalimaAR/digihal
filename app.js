7bfc3b52799ef9b104b5870c9a42d8685f92ced4
Halima Arredouani <h.arredouani@gmail.com>
2020-11-24 20:36:34 +0100

    final website

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const ejs = require('ejs');
+const app = express();
+const port = process.env.PORT
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render("home")
+});
+
+app.get('/over', (req, res) => {
+  res.render("over")
+});
+app.get('/diensten', (req, res) => {
+  res.render("diensten")
+});
+
+app.get('/team', (req, res) => {
+  res.render("team")
+});
+
+app.get('/contact', (req, res) => {
+  res.render("contact")
+})
+
+app.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`)
+});
