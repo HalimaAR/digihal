ace49b828f5dd274e2084edda6bf74e292e5b120
Halima Arredouani <h.arredouani@gmail.com>
2020-12-07 21:19:15 +0100

    CSS update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const ejs = require('ejs');
 const app = express();
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
